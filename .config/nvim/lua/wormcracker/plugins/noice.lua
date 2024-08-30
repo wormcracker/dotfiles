8dd24f965876d54cac1c9db517b55dad80489094
wormcracker <wormcrackerz@gmial.com>
2024-08-30 21:58:56 +0545

    Update

--- .config/nvim/lua/wormcracker/plugins/noice.lua
+++ .config/nvim/lua/wormcracker/plugins/noice.lua
@@ -2,32 +2,56 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	opts = {
-		-- add any options here
+		notify = {
+			render = "default",
+			timeout = 3000,
+			stages = "fade_in_slide_out",
+		},
 	},
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper module="..." entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   nvim-notify is only needed, if you want to use the notification view.
-		--   If not available, we use mini as the fallback
 		"rcarriga/nvim-notify",
 	},
+	config = function()
+		require("notify").setup({
+			stages = "fade_in_slide_out",
+			timeout = 3000,
+			render = "default",
+			background_colour = "Normal",
+			fps = 30,
+			top_down = false, -- This moves the notifications to the bottom
+			position = "bottom_right", -- Set position to bottom right
+
+			-- Dynamically adjust width and height based on screen size
+			max_width = function()
+				-- Dynamic width based on screen columns
+				return math.floor(vim.o.columns * 1)
+			end,
+			max_height = function()
+				-- Dynamic height based on screen lines
+				return math.floor(vim.o.lines * 0.3)
+			end,
+		})
+
+		-- Set Noice to use nvim-notify if available
+		require("noice").setup({
+			notify = require("notify"),
+		})
+	end,
 	setup = {
 		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				["cmp.entry.get_documentation"] = true,
 			},
 		},
-		-- you can enable a preset for easier configuration
 		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+			bottom_search = true,
+			command_palette = true,
+			long_message_to_split = true,
+			inc_rename = false,
+			lsp_doc_border = false,
 		},
 	},
 }
